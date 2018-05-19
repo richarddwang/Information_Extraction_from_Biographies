8e35a8de8cb9be0fc7f6e217544d3d3b2db4e30c
shulin8518 <frankie8518@gmail.com>
2018-05-19 08:14:52 +0800

    [Modify] modify preprocess way  and restruct code

--- Preprocess.py
+++ Preprocess.py
@@ -2,19 +2,17 @@ import re
 import json
 import os
 import subprocess
+import math
 
 def main():
     #
     biographies = get_biographies()
-    for (i, biography) in enumerate(biographies):
-        text = process_text(biographies, i)
-        name = biographies[i]["Name"]
-        with open("./DataBase/mature_txt/{}.txt".format(name), 'w') as f:
-            f.write(text)
-
+    for biography in biographies:
+        process_biograpy(biography)
+        
     #
     output_names(biographies)
-
+    
 def get_biographies():
     json_file_s = list(filter(lambda fle: fle[-5:]=='.json', # 取後面4個字是.json 的
                                   os.listdir('./DataBase/metaData'))) # 從./Texts 底下所有檔案或目錄取
@@ -28,12 +26,36 @@ def get_biographies():
             
     return biographies
 
-def process_text(biographies, i):
-    name = biographies[i]["Name"]
+def process_biograpy(biography):
+    name = biography["Name"]
+    startPage = biography["StartPage"]
 
-    with open('./DataBase/raw_txt/{}.txt'.format(name), 'r') as f:
+    with open('./DataBase/raw_txt/{}-{}.txt'.format(startPage, name), 'r') as f:
         text = f.read()
 
+    #
+    text, footnote_indices = process_text(text)
+
+    #
+    content, footnote = distinguish_footnote(text)
+
+    #
+    content = paragraph_clarify(content)
+    footnote = paragraph_clarify(footnote)
+
+    #
+    process_footnote(footnote, biography)
+
+    #
+    content = process_content(content, biography, footnote_indices)
+
+    # Output
+    with open('./DataBase/mature_txt/{}-{}.txt'.format(startPage, name), 'w') as f:
+        f.write(content)
+    with open('./DataBase/metaData/{}-{}.json'.format(startPage, name), 'w') as f:
+        json.dump(biography, f)
+
+def process_text(text):
     #
     match = re.search(r'^(第\w章)　(\w+)$', text, flags=re.MULTILINE)
     if match: #
@@ -43,57 +65,87 @@ def process_text(biographies, i):
         text = text.replace("{}\n{}\n".format(category, chapter_th), "")
 
     #
-    page_s = re.split(r'\d \d \d', text)[1:]
-    all_comment_indices = []
-    page_content_s = []
-    for page in page_s:
-        comment_indices = re.findall(r'\n(\d+) ', page)
-        all_comment_indices += comment_indices
-        if comment_indices: #
-            splits = re.split(r'\n\d+ ', page)
-            content = splits[0]
-            footnotes = splits[1:]
-            biographies[i]["footnotes"] = zip(comment_indices, footnotes)
-            page_content_s.append(content)
-        else:
-            page_content_s.append(page)
-    text = ''.join(page_content_s)
-    
+    text = re.sub(r'^(\d) (\d) (\d)$', '\g<1>Ä\g<2>Ä\g<3>', text ,flags=re.MULTILINE)
+    text = re.sub(r'([a-zA-Z,]) ([a-zA-Z,])', '\g<1>Ä\g<2>', text)
+    text = re.sub(r'(\n\d+) ', '\g<1>Ä', text)
+    text = text.replace(" ","")
+    text = text.replace("Ä", " ")
+
     #
-    for index in all_comment_indices:
-        # test if behind people name
-        text = text.replace("{} {}（".format(name, index), "{}（".format(name))
-        text = re.sub("([。，）])" + index, r'\g<1>', text)
+    footnote_indices = re.findall(r'\n(\d+) \w\w', text)
 
-    # deal with newline
-    text = re.sub(r'。\n', "。。。", text, re.MULTILINE)
-    text = re.sub(r'\n', "", text)
-    text = re.sub(r'。。。', "。\n\n", text, re.MULTILINE)
+    return text, footnote_indices
 
-    # remove space before number
-    text = re.sub(r' +(\d)', r'\g<1>', text)
+def distinguish_footnote(text):
+    page_s = re.split(r'\d \d \d', text)
+    content_part_s = []
+    footnote_part_s = []
+    for page in page_s:
+        cut_at = math.inf
 
-    # remove author
-    match = re.search(r"（([\w+、]+)撰寫?）", text, flags=re.MULTILINE) # $
-    author_line = match[0]
-    authorGroup = match[1]
-    text = text.replace(author_line, "")
-    biographies[i]["Authors"] = authorGroup.split("、")
+        match = re.search(r'^\d+ ', page , flags=re.MULTILINE)
+        if match:
+            mStart, mEnd = match.span()
+            cut_at = mStart
 
-    # remove title of biography, and get birth and death
-    reg = name + "（(.+，)?([\d?.]*)-([\d?.]*)）"
-    title = re.search(reg, text, flags=re.MULTILINE)
-    if len(title.groups()) == 2:
-        biographies[i]["Birth"] = title[1]
-        biographies[i]["Death"] = title[2]
-    else:
-        biographies[i]["Alias_s"].append(title[1])
-        biographies[i]["Birth"] = title[2]
-        biographies[i]["Death"] = title[3]
-    
-    text = text.replace(title[0], "")
+        match = re.search(r'^.+，(頁[\d-]+|第[\d-]+版)。$',page ,flags=re.MULTILINE)
+        if match:
+            mStart, mEnd = match.span()
+            cut_at = min(mStart, cut_at)
+
+        if cut_at is not math.inf:
+            content_part = page[:cut_at]
+            footnote_part = page[cut_at:]
+            content_part_s.append(content_part)
+            footnote_part_s.append(footnote_part)
+        else:
+            content_part_s.append(page)
+            
+    content_text = "".join(content_part_s)
+    footnote_text = "".join(footnote_part_s)
+
+    return content_text, footnote_text
+
+def paragraph_clarify(text):
+    text = text.replace("。\n", "Å")
+    text = text.replace("\n", "")
+    text = text.replace("Å", "。\n\n")
 
     return text
+
+def process_footnote(footnote, biography):
+    footnote = footnote[:-2]
+    f_lines = footnote.split('\n\n')
+    biography["Footnotes"] = list(map(lambda line: line.split(" "), f_lines))
+
+def process_content(content, biography, footnote_indices):
+    name = biography["Name"]
+    
+    #
+    for index in footnote_indices:
+        # test if behind people name
+        content = content.replace("{}{}（".format(name, index), "{}（".format(name))
+        content = re.sub("([。，])" + index, r'\g<1>', content)
+
+    #
+    match = re.search(r'（([\w、]+)撰寫?）', content, flags=re.MULTILINE) # $
+    author_line = match[0]
+    biography["Authors"] = match[1].split("、")
+    content = content.replace(author_line, "")
+
+    #
+    reg = name + "（(.+，)?([\d?.]*)-([\d?.]*)）"
+    title = re.search(reg, content, flags=re.MULTILINE)
+    if len(title.groups()) == 2:
+        biography["Birth"] = title[1]
+        biography["Death"] = title[2]
+    else:
+        biography["Alias_s"].append(title[1])
+        biography["Birth"] = title[2]
+        biography["Death"] = title[3]
+    
+    content = content.replace(title[0], "")
+    return content
 
 def output_names(biographies):
     names = []
